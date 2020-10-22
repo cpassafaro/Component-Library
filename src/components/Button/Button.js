5d001428e062ff05717b775c8bf1bef049d46b33
christinapassafaro <christinapassafaro@gmail.com>
2020-10-22 11:54:21 -0400

    moved repo to github.com

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import './Button.css'
 
 // This is a functional component - just sent up a little differently as an arrow function!
-const Button = (props) => (
-    <button>
+const Button = (props) => {
+    let classList = '';
+
+    let types = ['primary', 'danger', 'success', 'warning']
+
+    if(types.includes(props.type)){
+        classList+= `button-${props.type}`
+    }
+    console.log(props.large)
+    if(props.large){
+        classList+= ` button-large`
+    }
+
+    if(props.outline){
+        classList = ` button-${props.type}-outline`
+    }
+  
+  return(
+    <button className={classList}>
         {props.label}
     </button>
-)
+  )
+}
 
 export default Button; 
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,5 +3,13 @@ import { storiesOf } from '@storybook/react';
 
 import Button from './Button';
 
+
 storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+    .add('Primary', () => <Button label="Primary" type='primary'/>)
+    .add('Success', () => <Button label="Success" type='success'/>) 
+    .add('Primary', () => <Button label="Large Primary Button" type='primary' large/>)
+
+
+
+    .add('Danger', () => <Button label="Danger" type='danger' outline/>) 
+    .add('Warning', () => <Button label="Warning" type='warning'/>) 
